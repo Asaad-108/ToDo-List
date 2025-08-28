b809970c9d7311f1d8ecd129342552e3eced4dbc
Asaad-108 <asadasghar836@gmail.com>
2025-08-28 15:48:51 +0500

    UI changes

--- app/src/main/java/com/example/todo_list/DummyDB.kt
+++ app/src/main/java/com/example/todo_list/DummyDB.kt
@@ -5,7 +5,7 @@ object DummyDB {
         val tasks= mutableListOf<Task>()
         for (i in 0 until 10){
             val temp1 = Task("Coding","Making To-do List")
-            val temp2 = Task("Gym","Leg day")
+            val temp2 = Task("Gym","Leg day and i also have a lot of work to do tommorrow is also my school and there is alot of homework left")
             tasks.add(temp1)
             tasks.add(temp2)
         }
