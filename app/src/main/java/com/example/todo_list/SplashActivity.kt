d3b42d2e381c16d2a2da9d6d23acfe02b5687361
Asaad-108 <asadasghar836@gmail.com>
2025-08-29 22:31:58 +0500

    Auth verification

--- app/src/main/java/com/example/todo_list/SplashActivity.kt
+++ app/src/main/java/com/example/todo_list/SplashActivity.kt
@@ -9,10 +9,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.coroutines.delay
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var auth:FirebaseAuth;
+    private lateinit var ref:DatabaseReference;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,12 +27,23 @@ class SplashActivity : AppCompatActivity() {
 //            insets
 //        }
         auth = FirebaseAuth.getInstance();
+        ref = FirebaseDatabase.getInstance().getReference("Users");
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            val i = Intent(this,LoginActivity::class.java);
-            startActivity(i);
-            finish();
-        },500)
+        val current:FirebaseUser?=auth.currentUser
+        if(current!=null){
+            val id=current.uid
+            Handler(Looper.getMainLooper()).postDelayed({
+                val i = Intent(this,MainActivity::class.java);
+                startActivity(i);
+                finish();
+            },500)
+        }
+        else{
+            Handler(Looper.getMainLooper()).postDelayed({
+                val i = Intent(this,LoginActivity::class.java);
+                startActivity(i);
+                finish();
+            },500)
+        }
     }
-
 }
